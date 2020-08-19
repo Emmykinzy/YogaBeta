6050f79ba47ea8c784f35a26479d3ece3ba840b0
Greg Johnson <johnson_viau@msn.com>
2020-08-19 17:24:41 -0400

    Added build stage componet, etc

---
+++ YogaBeta/Model/YogaClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace YogaBeta.Model
+{
+    public class YogaClass
+    {
+        public int PoseDuration { get; set; }
+
+        public int PrepDuration { get; set; }
+
+        public string Shavasana { get; set; }
+
+        public int ShavasanaLength { get; set; }
+
+        public int ClassDuration
+        { get { return (PoseDuration * 7) + ((PrepDuration * 6) / 60) + ShavasanaLength; }}
+
+        public Poses[] PoseList { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<YogaClass>(this);
+    }
+}
---
+++ YogaBeta/Pages/ClassConfirmation.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YogaBeta.Pages
+{
+    public class ClassConfirmationModel : PageModel
+    {
+        [BindProperty]
+        public int PoseDuration { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public ActionResult OnPost()
+        {
+            return Page();
+        }
+    }
+}
