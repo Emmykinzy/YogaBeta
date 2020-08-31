e0e75727d8837fdd84bf62fc4c7761b5b03cb7d9
ekjoh <ekjohnson99@gmail.com>
2020-08-31 18:30:23 -0400

    New Generator

---
+++ YogaBeta/Model/SlideShow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YogaBeta.Model
+{
+    public class SlideShow
+    {
+        public string Name { get; set; }
+
+        public string Picture { get; set; }
+
+        public decimal Time { get; set; }
+
+        public string Type { get; set; }
+    }
+}
